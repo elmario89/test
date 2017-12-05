a8311a7c1cd57636e8d51ae3d886dec5f3a27c69
Smirnov Mikhail <urine89@mail.ru>
2017-12-05 22:21:53 +0300

    Add whatwf fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     devtool: 'inline-source-map',
     entry: [
         'babel-polyfill',
+        'whatwg-fetch',
         'react-hot-loader/patch',
         'webpack-hot-middleware/client',
         './src/index',
